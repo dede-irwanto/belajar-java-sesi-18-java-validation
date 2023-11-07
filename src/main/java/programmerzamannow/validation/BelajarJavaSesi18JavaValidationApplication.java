cf29dcbca73935ed1c5590110d8bb3bc85ba4af5
dede-irwanto <id.dedeirwanto@gmail.com>
2023-11-07 15:53:01 +0800

    first commit

---
+++ src/main/java/programmerzamannow/validation/BelajarJavaSesi18JavaValidationApplication.java
@@ -0,0 +1,13 @@
+package programmerzamannow.validation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BelajarJavaSesi18JavaValidationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BelajarJavaSesi18JavaValidationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/programmerzamannow/validation/BelajarJavaSesi18JavaValidationApplicationTests.java
@@ -0,0 +1,13 @@
+package programmerzamannow.validation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BelajarJavaSesi18JavaValidationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
